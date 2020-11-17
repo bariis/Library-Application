a9b4f2635a56ae0b66734e0d021e42c06ffff444
bariis <ertasbariis@gmail.com>
2020-11-17 13:08:45 +0300

    first commit

---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/LibraryApplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.baris.ertas.LibraryApplicaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplicatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplicatonApplication.class, args);
+	}
+
+}
