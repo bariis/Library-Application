afac0e832cf578344ef87abf5355b680451bbab7
bariis <ertasbariis@gmail.com>
2020-11-21 03:32:52 +0300

    user repository added

---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.baris.ertas.LibraryApplicaton.repository;
+
+import com.baris.ertas.LibraryApplicaton.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
+
+}
