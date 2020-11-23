d9c67806a407009cc4a08e93eb8b4541559bb4c8
bariis <ertasbariis@gmail.com>
2020-11-24 00:56:08 +0300

    role entity added

---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/model/Role.java
@@ -0,0 +1,48 @@
+package com.baris.ertas.LibraryApplicaton.model;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role(String name, Set<User> users) {
+        this.name = name;
+        this.users = users;
+    }
+
+    public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
