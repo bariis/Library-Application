2650e641dd4bec7978f573f50d6262849cb89295
bariis <ertasbariis@gmail.com>
2020-11-24 00:59:21 +0300

    user, role repos added

---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.baris.ertas.LibraryApplicaton.repository;
+
+import com.baris.ertas.LibraryApplicaton.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.baris.ertas.LibraryApplicaton.repository;
+
+import com.baris.ertas.LibraryApplicaton.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+}
