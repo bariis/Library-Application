0ef7085af222afc7668dd79e416be5f6a8d9d854
bariis <ertasbariis@gmail.com>
2020-11-24 00:58:34 +0300

    default admin added

--- src/main/java/com/baris/ertas/LibraryApplicaton/LibraryApplicatonApplication.java
+++ src/main/java/com/baris/ertas/LibraryApplicaton/LibraryApplicatonApplication.java
@@ -3,14 +3,17 @@ package com.baris.ertas.LibraryApplicaton;
 import com.baris.ertas.LibraryApplicaton.model.Author;
 import com.baris.ertas.LibraryApplicaton.model.Book;
 import com.baris.ertas.LibraryApplicaton.model.Publisher;
+import com.baris.ertas.LibraryApplicaton.model.User;
 import com.baris.ertas.LibraryApplicaton.repository.AuthorRepository;
 import com.baris.ertas.LibraryApplicaton.repository.BookRepository;
 import com.baris.ertas.LibraryApplicaton.repository.PublisherRepository;
+import com.baris.ertas.LibraryApplicaton.service.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -18,14 +21,20 @@ import java.util.Set;
 @SpringBootApplication
 public class LibraryApplicatonApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(LibraryApplicatonApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplicatonApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner mappingDemo(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+	public CommandLineRunner mappingDemo(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository, UserServiceImpl userServiceImpl) {
 
 		return args -> {
+
+			User adminUser = new User("admin", "admin123", new HashSet<>());
+
+			userServiceImpl.saveAdmin(adminUser);
 
 			Author author = new Author("schopenhauer", "alman felsefeci");
 			Publisher publisher = new Publisher("can yayinlari", "bura can yayinlari");
