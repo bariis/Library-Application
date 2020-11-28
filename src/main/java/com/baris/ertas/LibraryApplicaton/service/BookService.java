a44bad95f6716acec4ba64c2e447fe5ff0673b41
bariis <ertasbariis@gmail.com>
2020-11-28 23:46:33 +0300

    book service refactored

--- src/main/java/com/baris/ertas/LibraryApplicaton/service/BookService.java
+++ src/main/java/com/baris/ertas/LibraryApplicaton/service/BookService.java
@@ -3,12 +3,17 @@ package com.baris.ertas.LibraryApplicaton.service;
 
 import com.baris.ertas.LibraryApplicaton.model.Author;
 import com.baris.ertas.LibraryApplicaton.model.Book;
+import com.baris.ertas.LibraryApplicaton.model.Publisher;
 import com.baris.ertas.LibraryApplicaton.repository.AuthorRepository;
 import com.baris.ertas.LibraryApplicaton.repository.BookRepository;
+import com.baris.ertas.LibraryApplicaton.repository.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,6 +22,7 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+
     public List<Book> findAllBooks() {
         List<Book> books = bookRepository.findAll().stream().collect(Collectors.toList());
         return books;
@@ -24,6 +30,15 @@ public class BookService {
 
     public Book getBookByName(String bookName) {
         return bookRepository.findByBookName(bookName);
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+
     }
 
 }
