28e0d7ef75ef7343bc9271c51178bf430218cb94
bariis <ertasbariis@gmail.com>
2020-11-28 22:25:40 +0300

    author, publisher services added

---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/service/AuthorService.java
@@ -0,0 +1,24 @@
+package com.baris.ertas.LibraryApplicaton.service;
+
+import com.baris.ertas.LibraryApplicaton.model.Author;
+import com.baris.ertas.LibraryApplicaton.model.Book;
+import com.baris.ertas.LibraryApplicaton.repository.AuthorRepository;
+import com.baris.ertas.LibraryApplicaton.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> findAllAuthors() {
+        List<Author> authors = authorRepository.findAll().stream().collect(Collectors.toList());
+        return authors;
+    }
+
+}
---
+++ src/main/java/com/baris/ertas/LibraryApplicaton/service/PublisherService.java
@@ -0,0 +1,23 @@
+package com.baris.ertas.LibraryApplicaton.service;
+
+import com.baris.ertas.LibraryApplicaton.model.Author;
+import com.baris.ertas.LibraryApplicaton.model.Publisher;
+import com.baris.ertas.LibraryApplicaton.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PublisherService {
+
+    @Autowired
+    PublisherRepository publisherRepository;
+
+    public List<Publisher> findAllPublishers() {
+        List<Publisher> publishers = publisherRepository.findAll().stream().collect(Collectors.toList());
+        return publishers;
+    }
+
+}
