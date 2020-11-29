6e3198ea42891518b90df003c7aaa5ceaadfd6ad
bariis <ertasbariis@gmail.com>
2020-11-29 22:30:10 +0300

    update operation done

--- src/main/java/com/baris/ertas/LibraryApplicaton/controller/BookController.java
+++ src/main/java/com/baris/ertas/LibraryApplicaton/controller/BookController.java
@@ -1,14 +1,18 @@
 package com.baris.ertas.LibraryApplicaton.controller;
 
+import com.baris.ertas.LibraryApplicaton.model.Author;
 import com.baris.ertas.LibraryApplicaton.model.Book;
+import com.baris.ertas.LibraryApplicaton.model.Publisher;
+import com.baris.ertas.LibraryApplicaton.service.AuthorService;
 import com.baris.ertas.LibraryApplicaton.service.BookService;
+import com.baris.ertas.LibraryApplicaton.service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -17,9 +21,15 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private PublisherService publisherService;
+
+    @Autowired
+    private AuthorService authorService;
+
     // tek bir kitabi gosterir
     @GetMapping( path = "books/show/{id}")
-    public String showSingleBook(@PathVariable("id") long id, Model model) {
+    public String showSingleBook(@PathVariable("id") Long id, Model model) {
         Optional<Book> viewedBook = bookService.getBookById(id);
         if(viewedBook.isPresent()) {
             model.addAttribute("viewedBook", viewedBook);
@@ -28,5 +38,32 @@ public class BookController {
 
         return "Boyle bir kitap yok sorun olustu.";
     }
+
+    @GetMapping(path = "books/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        Optional<Book> viewedBook = bookService.getBookById(id);
+        if(viewedBook.isPresent()) {
+            System.out.println("view edilen book -> " + viewedBook.toString());
+            List<Author> authorList = new ArrayList<>();
+            List<Publisher> publisherList = new ArrayList<>();
+            authorList = authorService.findAllAuthors();
+            publisherList = publisherService.findAllPublishers();
+            model.addAttribute("viewedBook", viewedBook);
+            model.addAttribute("authors", authorList);
+            model.addAttribute("publishers", publisherList);
+            return "editBook";
+        }
+
+        return "Boyle bir kitap yok sorun olustu.";
+    }
+
+
+    //@PutMapping(path = "books/update/{id}")
+    @RequestMapping(path = "books/edit/{id}", method = RequestMethod.POST)
+    public String updateBook(@PathVariable("id") Long id, Book book) {
+        bookService.updateBook(id, book);
+        return "home";
+    }
+
 
 }
